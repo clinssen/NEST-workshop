3842d0d1e21b151f3b0f72293dc94446906e9deb
clinssen <c.linssen@fz-juelich.de>
2025-07-02 22:10:00 +0200

    update repository URL for tutorial materials

--- docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
+++ docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
@@ -5,7 +5,7 @@ source /opt/nest/bin/nest_vars.sh
 
 
 cd /tmp
-wget https://raw.githubusercontent.com/clinssen/OCNS-2024-NEST-workshop/master/docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
+wget https://raw.githubusercontent.com/clinssen/NEST-workshop/master/docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
 mkdir $HOME/.jupyter
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_lab_config.py
@@ -16,9 +16,9 @@ git clone https://github.com/nest/nestml
 
 jupyter-lab &
 
-#mv -f OCNS-2024-NEST-workshop /tmp/
-#git clone https://github.com/clinssen/OCNS-2024-NEST-workshop
-#cd OCNS-2024-NEST-workshop/materials
+#mv -f NEST-workshop /tmp/
+#git clone https://github.com/clinssen/NEST-workshop
+#cd NEST-workshop/materials
 #mv nestml /tmp/orig-workshop-NESTML-materials
 #ln -s ../../nestml/doc/tutorials nestml
 #cd ..
